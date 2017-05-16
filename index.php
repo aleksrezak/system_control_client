cebc04316e8a727ec9acfc589642ad07f3c67c37
aleksrezak <aleks20142805@icloud.com>
2017-05-16 21:25:27 +0300

    таблицы

--- index.php
+++ index.php
@@ -2,9 +2,59 @@
 <html>
 <head>
 <meta charset="utf-8">
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js""></script>
 <link rel="stylesheet" type="text/css" href="style.css">
 	<title>Умный шкаф</title>
 </head>
+<script type="text/javascript">
+
+$(document).ready(function()
+{
+
+    var counter = 2;
+
+    $("#addButton").click(function () {
+
+	if(counter>10){
+            alert("Only 10 textboxes allow");
+            return false;
+	}
+
+	var newTextBoxDiv = $(document.createElement('div'))
+	     .attr("id", 'TextBoxDiv' + counter);
+
+	newTextBoxDiv.after().html('<label>'+ counter + ' : </label>' +
+	      '<input type="text" name="textbox' + counter +
+	      '" id="textbox' + counter + '" value="" >');
+
+	newTextBoxDiv.appendTo("#TextBoxesGroup");
+
+
+	counter++;
+     });
+
+     $("#removeButton").click(function () {
+	if(counter==1){
+          alert("No more textbox to remove");
+          return false;
+       }
+
+	counter--;
+
+        $("#TextBoxDiv" + counter).remove();
+
+     });
+
+     $("#getButtonValue").click(function () {
+
+	var msg = '';
+	for(i=1; i<counter; i++){
+   	  msg += "\n" + i + " : " + $('#textbox' + i).val();
+	}
+    	  alert(msg);
+     });
+  });
+</script>
 <body>
 <header>
 	<div id ="logo">
@@ -12,7 +62,7 @@
 	</div>
 
 	<div id= "menuHead">
-		<a href="D:/SCK/about.php">
+		<a href="about.php">
 			<div style="margin-right: 5%">О нас</div> 
 		</a>
 		<a href="feedback.php">
@@ -24,7 +74,17 @@
 		<a href ="regAuth.php">Регистрация</a>|<a href="auth.php">Авторизация
 		</a>
 	</div>
-</header>
+	</header>
+
+<div id='TextBoxesGroup'>
+	<div id="TextBoxDiv1">
+		<label>1: </label><input type='textbox' id='textbox1' >
+	</div>
+</div>
+<input type='button' value='Добавить' id='addButton'>
+<input type='button' value='Удалить ' id='removeButton'>
+
+
 <footer>
 	<div id = "social">
 	</div>
